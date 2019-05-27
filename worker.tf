1ecb345600e3f552c5118c0100bac4fac98fd220
Chin Dou <cdou.swsec@gmail.com>
2019-05-26 21:25:28 -0400

    feat(worker): put back network_security_group_id and load_balancer_backend_address_pools_ids

--- worker.tf
+++ worker.tf
@@ -3,11 +3,13 @@ resource "azurerm_network_interface" "worker" {
   name                      = "${var.cluster_name}-${var.environment}-${var.name_suffix}-${format("worker%d", count.index + 1)}"
   location                  = "${data.azurerm_resource_group.main.location}"
   resource_group_name       = "${data.azurerm_resource_group.main.name}"
+  network_security_group_id = "${var.network_security_group_id}"
 
   ip_configuration {
     name                                    = "${var.cluster_name}-${var.environment}-${var.name_suffix}-${format("worker%d", count.index + 1)}"
     subnet_id                               = "${data.azurerm_subnet.subnet.id}"
     private_ip_address_allocation           = "dynamic"
+    load_balancer_backend_address_pools_ids = ["${var.lb_address_pool_id}"]
   }
 }
 
